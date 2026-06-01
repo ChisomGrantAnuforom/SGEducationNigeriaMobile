e20ea7f3de815753e22bc5cb96cd8cc1ea2a0639
ChisomGrantAnuforom <chisomgrantnufor@gmail.com>
2026-06-01 04:06:17 +0100

    Commit "ios cert updates"

--- UserShell.xaml.cs
+++ UserShell.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AndroidX.AppCompat.App;
+// using AndroidX.AppCompat.App;
 using SGEducationNigeriaMobile.Helpers;
 using SGEducationNigeriaMobile.Pages;
 using SGEducationNigeriaMobile.Pages.User;
